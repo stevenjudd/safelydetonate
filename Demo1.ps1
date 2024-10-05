343f43aea32b62f9f01a763f9c945065d32b0ab2
Steven Judd <stevenjudd@users.noreply.github.com>
2024-10-05 09:50:43 -0500

    adding commands to show the container status

--- Demo1.ps1
+++ Demo1.ps1
@@ -1,4 +1,11 @@
+Write-Host 'This demo assumes you have Docker installed' -ForegroundColor Cyan
 Wait-Debugger
 docker pull jlesage/firefox
 docker run -d --name=firefox -p 5800:5800 -v /docker/appdata/firefox:/config:rw jlesage/firefox
 # http://127.0.0.1:5800
+docker ps
+docker ps -q --filter 'ancestor=jlesage/firefox' | ForEach-Object { docker stop $_ }
+docker ps
+docker ps -a
+docker container prune
+docker ps -a
